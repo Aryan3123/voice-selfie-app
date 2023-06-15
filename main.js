62a86c601e907d6c84a74a8e0f9023c5d1c14c83
Aryan3123 <107537210+Aryan3123@users.noreply.github.com>
2023-06-15 10:50:00 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -25,12 +25,19 @@ recognition.onresult = function(event) {
 function speak(){
     var synth = window.speechSynthesis;
 
-    speak_data = document.getElementById("textbox").value;
+    speak_data = "Taking your Selfie in 5 seconds";
 
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     
     synth.speak(utterThis);
     Webcam.attach(camera);
+
+    setTimeout(function()
+    {
+        take_snapshot();
+        save();
+    }, 5000);
+
 }
 
 Webcam.set({
@@ -40,3 +47,43 @@ Webcam.set({
     png_quality:90
 });
 camera = document.getElementById("camera");
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_url) {
+        document.getElementById("result").innerHTML = '<img id="selfie_image" src="'+data_url+'">';
+    });
+}
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+
+recognition.onresult = function(event) {
+
+    console.log(event);
+
+    var Content = event.results[0][0].transcript;
+
+    document.getElementById("textbox").innerHtml = Content;
+    console.log(Content);
+    if(Content =="take my selfie")
+    {
+        console.log("taking selfie --- ");
+        speak();
+    }
+}
+
+function save()
+{
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src ;
+    link.href = image;
+    link.click();
+}
